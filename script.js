1a7dc433a9731af3b43fb9df567a205362aa3e4f
nwokiwu <51241053+nwokiwu@users.noreply.github.com>
2019-07-18 16:30:13 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
-$.ajax({
-    url: 'https://randomuser.me/api/',
-    dataType: 'json',
-    success: function (data) {
-        console.log(data);
-    }
-});
+// $.ajax({
+//     url: 'https://randomuser.me/api/',
+//     dataType: 'json',
+//     success: function (data) {
+//         console.log(data);
+//     }
+// });
+
+const RandomUser = 'https://randomuser.me/api/';
+const search = "search-container";
+const images = "gallery";
+
+// console.log(images); Tested the code
+
+function getJSON(url)
+
